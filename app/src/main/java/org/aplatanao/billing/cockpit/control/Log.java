2a429c07023b4d79e839eec223d90e51210f58ba
andre <andrzimmermann@gmx.de>
2018-08-11 12:53:44 +0100

    rebuild log extends log table and push the data

--- app/src/main/java/org/aplatanao/billing/cockpit/control/Log.java
+++ app/src/main/java/org/aplatanao/billing/cockpit/control/Log.java
@@ -1,15 +1,25 @@
 package org.aplatanao.billing.cockpit.control;
 
-import javafx.scene.control.TextArea;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
-public class Log extends TextArea {
+public class Log extends LogTable {
+
+    private final ObservableList<org.aplatanao.billing.cockpit.models.Log> data = FXCollections.observableArrayList();
 
     private void log(String level, String message) {
-        appendText(String.format("%s %s: %s\n", LocalDate.now(), level, message));
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+
+        data.add(new org.aplatanao.billing.cockpit.models.Log(dateFormat.format(date), level, message));
+        setItems(data);
     }
 
     public void trace(String message) {
