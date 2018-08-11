2e40b20dec4321118eb1401e5361cad3de2808f5
andre <andrzimmermann@gmx.de>
2018-08-11 12:49:09 +0100

    create model for the log table

---
+++ app/src/main/java/org/aplatanao/billing/cockpit/models/Log.java
@@ -0,0 +1,40 @@
+package org.aplatanao.billing.cockpit.models;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Log {
+    private final SimpleStringProperty dateCol;
+    private final SimpleStringProperty eventCol;
+    private final SimpleStringProperty messageCol;
+
+    public Log(String cDate, String cEvent, String cMsg) {
+        this.dateCol = new SimpleStringProperty(cDate);
+        this.eventCol = new SimpleStringProperty(cEvent);
+        this.messageCol = new SimpleStringProperty(cMsg);
+    }
+
+    public String getDateCol() {
+        return dateCol.get();
+    }
+
+    public void setDateCol(String dateCol) {
+        this.dateCol.set(dateCol);
+    }
+
+    public String getEventCol() {
+        return eventCol.get();
+    }
+
+    public void setEventCol(String eventCol) {
+        this.eventCol.set(eventCol);
+    }
+
+    public String getMessageCol() {
+        return messageCol.get();
+    }
+
+    public void setMessageCol(String messageCol) {
+        this.messageCol.set(messageCol);
+    }
+
+}
