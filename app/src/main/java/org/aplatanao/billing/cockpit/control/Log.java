2bd4ea0b705d8813d0620679feb1dc20db895014
andre <andrzimmermann@gmx.de>
2018-08-17 23:40:55 +0100

    add toolbar whith filter in control log

--- app/src/main/java/org/aplatanao/billing/cockpit/control/Log.java
+++ app/src/main/java/org/aplatanao/billing/cockpit/control/Log.java
@@ -2,20 +2,34 @@ package org.aplatanao.billing.cockpit.control;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToolBar;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 import org.aplatanao.billing.cockpit.models.LogEntry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import java.time.format.DateTimeFormatter;
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
-public class Log extends TableView {
+public class Log extends VBox {
 
-    private final ObservableList<LogEntry> data = FXCollections.observableArrayList();
+    private ToolBar toolBar = new ToolBar();
+    private ComboBox levelFilter = new ComboBox();
+    private TableView tableView = new TableView();
 
+    private ObservableList<LogEntry> data = FXCollections.observableArrayList();
+
+    @Autowired
     public Log() {
+
+        levelFilter.getItems().addAll("TRACE", "INFO", "WARN", "ERROR");
+        toolBar.getItems().addAll(levelFilter);
+
         TableColumn date = new TableColumn("Date");
         date.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("date"));
         date.prefWidthProperty().bind(widthProperty().multiply(0.2));
@@ -28,14 +42,15 @@ public class Log extends TableView {
         message.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("message"));
         message.prefWidthProperty().bind(widthProperty().multiply(0.7));
 
-        getColumns().addAll(date, level, message);
+        tableView.getColumns().addAll(date, level, message);
+        getChildren().addAll(toolBar, tableView);
     }
 
     private void log(String level, String message) {
         data.add(
             new LogEntry(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), level, message)
         );
-        setItems(data);
+        tableView.setItems(data);
     }
 
     public void trace(String message) {
